5b543277d6ad9a22ed33d6a008a16865f051a352
Hossein <hosseinyaghmaee@gmail.com>
2021-02-18 18:59:03 +0330

    actions

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,9 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::resource("permissions","PermissionController");
+Route::resource("roles","RoleController");
+Route::post("roles/{role}/syncPermissions","RoleController@syncPermissions")->name("roles.syncPermissions");
+Route::post("roles/{role}/addUser","RoleController@addUser")->name("roles.addUser");
+Route::delete("roles/{role}/removeUser","RoleController@removeUser")->name("roles.removeUser");
